cd00ab02662f66cab22096c43e4b66f31c3e469f
sylvain kamdem <sylvain.kamdem@gmail.com>
2020-02-06 21:20:20 -0500

    completed exercise on Enum Practice

---
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -0,0 +1,28 @@
+package org.launchcode.enumerableplanets.data;
+
+/**
+ * Created by kamdem
+ */
+public enum Planets {
+    MERCURY("Mercury", 88, 0),
+    VENUS("Venus", 225, 0),
+    EARTH("Earth", 365, 1),
+    MARS("Mars", 687, 2),
+    JUPITER("Jupiter", 4333, 79),
+    SATURN("Saturn", 10759, 82),
+    URANUS("Uranus", 30687, 27),
+    NEPTUNE("Neptune", 60200, 14);
+
+    private final String displayName;
+    private final int yearLength;
+    private final int moons;
+
+    Planets(String displayName, int yearLength, int moons){
+        this.displayName = displayName;
+        this.yearLength = yearLength;
+        this.moons = moons;
+    }
+    public String getDisplayName() { return displayName;  }
+    public int getYearLength() { return yearLength; }
+    public int getMoons() { return moons;  }
+}
